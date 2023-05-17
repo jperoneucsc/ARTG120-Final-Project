bead495ea373ef16be85673dd28aa9ad9addadb0
jperone <jperone@ucsc.edu>
2023-05-16 18:24:48 -0700

    Could not get tilemap working, will try again tmr.

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -8,6 +8,13 @@ export class Scene1 extends Phaser.Scene {
     {
         // Load panda sprite sheet
         this.load.atlas('panda1', 'assets/panda.png', 'assets/panda.json');
+        this.load.image("tiles", "assets/gameMapTemp.png");
+
+        //Load sky
+        this.load.image("sky", "assets/skytemp.png");
+        this.load.image("ground", "assets/groundTemp.png");
+
+
     }
 
     create()
@@ -16,8 +23,19 @@ export class Scene1 extends Phaser.Scene {
         const width = this.scale.width;
         const height = this.scale.height;
 
+
+        // get background
+        let sky = this.add.image(width * 0.5, height * 0.5, 'sky').setScale(3);
+
+        const platforms = this.physics.add.staticGroup();
+        platforms.create(400,500, "ground").setScale(0.5).refreshBody();
+        // load map
+
+
         // Create Panda
-        // this.add.image(width * 0.5, height * 0.5, 'panda1', "panda_01_run_02.png");
+        let panda1 = this.add.image(width * 0.5, height * 0.5, 'panda1', "panda_01_run_02.png");
+        panda1.scale = 0.5
+
     }
 
 }
