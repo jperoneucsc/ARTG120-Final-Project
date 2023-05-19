3825b329d027742c46741892ea945478d2521fa3
jperone <jperone@ucsc.edu>
2023-05-19 00:43:59 -0700

    Added basic player movement and player animations

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -9,8 +9,11 @@ export class Scene1 extends Phaser.Scene {
         // Load panda sprite sheet
         this.load.atlas('panda1', 'assets/panda.png', 'assets/panda.json');
 
-        //Load sky
+        // Load background
         this.load.image("background", "assets/forestBackground.png");
+
+        // Load ground
+        this.load.image("ground", "assets/forestFloor.png")
 
 
     }
@@ -21,20 +24,124 @@ export class Scene1 extends Phaser.Scene {
         const width = this.scale.width;
         const height = this.scale.height;
 
+        // Create animations ------------------------------------------------
+        this.anims.create({
+            key: 'player-idle',
+            frameRate: 2,
+            frames: this.anims.generateFrameNames('panda1', {
+                start: 1,
+                end: 4,
+                prefix: 'panda_01_idle_0',
+                suffix: '.png'
+            }),
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'player-walk',
+            frameRate: 10,
+            frames: this.anims.generateFrameNames('panda1', {
+                start: 1,
+                end: 8,
+                prefix: 'panda_01_walk_0',
+                suffix: '.png'
+            }),
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'player-run',
+            frameRate: 10,
+            frames: this.anims.generateFrameNames('panda1', {
+                start: 1,
+                end: 5,
+                prefix: 'panda_01_run_0',
+                suffix: '.png'
+            }),
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'player-jump',
+            frameRate: 1,
+            frames: this.anims.generateFrameNames('panda1', {
+                start: 1,
+                end: 5,
+                prefix: 'panda_01_run_0',
+                suffix: '.png'
+            }),
+            repeat: -1
+        })
 
+        // Instantiate sprites + background + foreground ---------------------------------------
+        
         // get background
         let background1 = this.add.image(width * 0.5, height * 0.5, 'background');
 
-        //const platforms = this.physics.add.staticGroup();
-        //platforms.create(400,500, "ground").setScale(1).refreshBody();
-        // load map
-
+        // Create platforms to walk on
+        const platforms = this.physics.add.staticGroup();
+        platforms.create(width*0.5, height * .95, "ground").setScale(1);
+        platforms.create((width + width*0.5), height * .95, "ground").setScale(1).refreshBody();
 
         // Create Panda
-        let panda1 = this.add.image(width * 0.5, height * 0.5, 'panda1', "panda_01_run_02.png").setScale(0.5);
-
+        this.player = this.physics.add.sprite(width * 0.5, height * 0.5, 'panda1').setScale(0.3).play('player-idle');
+        // Add collider between panda and platforms
+        this.physics.add.collider(this.player, platforms);
     }
+    
+    update()
+    {  
+        // -------------------------------- PLAYER MOVEMENT ---------------------------------------
+        const cursors = this.input.keyboard.createCursorKeys();
 
+        // Check is player is able to jump
+        if(!cursors.up.isDown && this.player.body.touching.down){
+            this.player.allowedToJump = true;
+        }
+
+        // Check if player is pressing left or right, with shift or not
+        if(!this.player.body.touching.down){this.player.anims.play("player-jump", false)};
+        if (cursors.left.isDown){
+            if(cursors.shift.isDown){    // player is running
+                if(this.player.scaleX <= 0){
+                    this.player.scaleX = this.player.scaleX * -1;
+                }
+                this.player.setVelocityX(-240);
+                this.player.anims.play("player-run", true);
+            }else{      // player is walking
+                if(this.player.scaleX <= 0){
+                    this.player.scaleX = this.player.scaleX * -1;
+                }
+                this.player.setVelocityX(-160);
+                this.player.anims.play("player-walk", true);
+            }
+            
+        }else if (cursors.right.isDown){
+            if(cursors.shift.isDown){    // player is running
+                if(this.player.scaleX >= 0){
+                    this.player.scaleX = this.player.scaleX * -1;
+                }
+                this.player.setVelocityX(240);
+                this.player.anims.play("player-run", true);
+            }else{      // player is walking
+                if(this.player.scaleX >= 0){
+                    this.player.scaleX = this.player.scaleX * -1;
+                }
+                this.player.setVelocityX(160);
+                this.player.anims.play("player-walk", true);
+            }
+        }else{      // no key is being pressed
+            this.player.setVelocityX(0);
+            this.player.anims.play("player-idle", true);
+        }
+
+        // Check if player is trying to jump
+        if(cursors.up.isDown && this.player.body.touching.down && this.player.allowedToJump){
+            if (this.player.body.velocity.x == 0){
+                this.player.anims.play("player-jump");
+            }
+            this.player.setVelocityY(-600);
+            this.player.allowedToJump = false;
+        }
+
+        
+    }
 }
-
     
