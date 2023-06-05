f60cef6429f2da06cea8b24befa81b2790b7ef09
jperone <jperone@ucsc.edu>
2023-06-04 19:49:46 -0700

    Adding intro and fit to scale fix

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ let config ={
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
+    scale:{
+        mode:Phaser.Scale.FIT,
+        autoCenter:Phaser.Scale.CENTER_BOTH
+    },
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y:2000 },
-            //debug: true,
+            gravity: { y:2000 }
         }
     },
-    scene: [Scene1, Scene2]
+    scene: [Intro, Scene1, Scene2]
 };
 
 let game = new Phaser.Game(config);
